bb803f354b88e197070f52809d858ddb957711f3
SunTY <scarsty@gmail.com>
2018-01-15 13:12:24 +0800

    改名

---
+++ rename.sh
@@ -0,0 +1,22 @@
+#/usr/bash
+
+for file in *.png
+do
+newfile=$file
+#newfile=${newfile/__/_}
+newfile=${newfile/N_REVERSE__/}
+newfile=${newfile/Q_L__/}
+newfile=${newfile/N__/}
+newfile=${newfile/OK_/}
+newfile=${newfile/W_L_BB__/}
+newfile=${newfile/W_L_LQ__/}
+newfile=${newfile/W_L__/}
+newfile=${newfile/A_/}
+newfile=${newfile/B_/}
+newfile=${newfile/C_/}
+newfile=${newfile/D_/}
+newfile=${newfile/E_/}
+newfile=${newfile/F_/}
+echo $file '->' $newfile
+mv $file $newfile
+done
