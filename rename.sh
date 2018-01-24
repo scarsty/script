36001cc90390c8fb4d728fe38a5b430dec3ad22f
SunTY <scarsty@gmail.com>
2018-01-24 09:20:46 +0800

    可用

--- rename.sh
+++ rename.sh
@@ -1,23 +1,10 @@
 #/usr/bash
 
-for file in */*.png
+for file in `/usr/bin/find . -name *.png`
 do
-newfile=$1-$file
-#newfile=${newfile/__/_}
-newfile=${newfile/N_REVERSE__/}
-newfile=${newfile/Q_L__/}
-newfile=${newfile/N__/}
-newfile=${newfile/OK_/}
-newfile=${newfile/W_L_BB__/}
-newfile=${newfile/W_L_LQ__/}
-newfile=${newfile/W_L__/}
-newfile=${newfile/A_/}
-newfile=${newfile/B_/}
-newfile=${newfile/C_/}
-newfile=${newfile/D_/}
-newfile=${newfile/E_/}
-newfile=${newfile/F_/}
-newfile=${newfile/\//-}
+newfile=${newfile/.\//}
+newfile=${newfile//\//-}
+newfile=$1-$newfile
 echo $file '->' $newfile
 mv $file $newfile
 done
