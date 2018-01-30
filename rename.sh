bb04f6b351ac20748ce8fa842543d4ecd290b937
SunTY <scarsty@gmail.com>
2018-01-30 16:48:46 +0800

    处理空格

--- rename.sh
+++ rename.sh
@@ -1,11 +1,12 @@
 #/usr/bash
-
+IFS=$(echo -en "\n\b")
 for file in `/usr/bin/find . -name "*.png"`
 do
 newfile=$file
 newfile=${newfile/.\//}
 newfile=${newfile//\//-}
+newfile=${newfile// /}
 newfile=$1-$newfile
 echo $file '->' $newfile
-mv $file $newfile
+mv "$file" "$newfile"
 done
