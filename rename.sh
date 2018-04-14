d16a068b13d2949ced4dd4a4be112da100d130f9
sty <scarsty@gmail.com>
2018-04-15 00:12:23 +0800

    no message

--- rename.sh
+++ rename.sh
@@ -2,14 +2,19 @@
 IFS=$(echo -en "\n\b")
 for file in `/usr/bin/find . -name "*.png"`
 do
-newfile=$file
-newfile=${newfile/.\//}
-newfile=${newfile//\//-}
-newfile=${newfile// /}
-if [ $1 -eq "1" ]; then
-DATE=`/usr/bin/date +'%Y%m%d'`
-newfile=$DATE-$newfile
-fi
-echo $file '->' $newfile
-mv "$file" "$newfile"
+    newfile=$file
+    newfile=${newfile/.\//}
+    newfile=${newfile//\//-}
+    newfile=${newfile// /}
+    if [ $# -ge 1  ]; then
+        if [ $1 -eq "1" ]; then
+            DATE=`/usr/bin/date +'%Y%m%d'`
+            newfile=$DATE-$newfile
+        fi
+        if [ $1 -eq "2" ]; then
+            newfile=$2-$newfile
+        fi
+    fi
+    echo $file '->' $newfile
+    mv "$file" "$newfile"
 done
