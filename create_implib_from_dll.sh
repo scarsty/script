931200ae5c6f2d0895e545e649d4e328ce2166bd
sty <scarsty@gmail.com>
2021-03-09 10:16:41 +0800

    no message

--- create_implib_from_dll.sh
+++ create_implib_from_dll.sh
@@ -5,15 +5,17 @@ echo LIBRARY $1.dll > $1.def
 echo EXPORTS >> $1.def
 VSPATH="/c/Program Files (x86)/Microsoft Visual Studio 14.0/VC/bin"
 NUM=`"$VSPATH/dumpbin" //exports $1.dll | grep "number of functions" | awk '{print $1}'`
-NUM2=`echo $NUM +2 | bc -l`
-FUNCS=`"$VSPATH/dumpbin" //exports $1.dll | grep "ordinal hint" -A $NUM2 | awk '{if(NR>2)print $4}'`
+NUM2=`echo $NUM +2 | /usr/bin/bc -l`
+FUNCS=`"$VSPATH/dumpbin" //exports $1.dll | grep "RVA" -A $NUM2 | awk '{if(NR>2)print $4}'`
 echo $NUM functions.
+#echo $NUM2
+#echo $FUNCS
 
 COUNT=1
 for func in $FUNCS; do
   echo $func @$COUNT >> $1.def
   #echo $func >> $1.def
-  COUNT=`echo $COUNT+1|bc -l`
+  COUNT=`echo $COUNT+1|/usr/bin/bc -l`
 done
 
 rm $1.lib
